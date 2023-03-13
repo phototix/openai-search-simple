d58b439b810f475dd03cbee302c646158577c7d1
Brandon Chong <brandon@kkbuddy.com>
2023-03-13 14:03:12 +0800

    After generated images, store inside cache file and list down from the cache folder.

---
+++ list.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap.min.css"/>
+	<title>OpenAI Image Gen Test</title>
+</head>
+<style type="text/css">
+h1{font-size: 1em;}
+</style>
+<body>
+
+<div class="container" style="margin-top:20px;margin-bottom:20px;">
+
+	<h1>Generated Images</h1>
+
+	<div class="row">
+		<?php
+		$strLoopNumber=0;
+		$files = glob("cache/*.png");
+		usort($files, function($a, $b) {
+		    return filemtime($b) - filemtime($a);
+		});
+		foreach ($files as $fileName) { ?>
+
+			<div class="col-6 col-md-4" style="padding:5px;">
+				<a href="<?=$fileName?>" target="_blank"><img src="<?=$fileName?>" width="100%" style="width: 100%;"></a>
+			</div>
+
+		<?php } ?>
+
+		<hr style="border-bottom: 2px solid #000;margin-top: 20px;">
+
+	</div>
+
+	<a href="/" class="btn btn-success">Back to Home</a>
+
+</div>
