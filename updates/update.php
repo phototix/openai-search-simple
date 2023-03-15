c9bc549d73413cba5ae63494a76ecc9f00282fa9
Brandon Chong <brandon@kkbuddy.com>
2023-03-15 10:03:15 +0800

    Return version info in footer.

--- updates/update.php
+++ updates/update.php
@@ -1,4 +1,7 @@
 <?php
+echo "<center>";
 // This is the one-liner to use PHP codes to update the scripts in background.
 include("updates/latest/info.php");
+echo "Ver. ".$currentVersion;
+echo "</center>";
 ?>
